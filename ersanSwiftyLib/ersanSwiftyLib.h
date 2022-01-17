47882ccf1e3b5335424a37e2087d232f36f3b013
Ersan <ersan@ersans-mbp.mynet>
2022-01-17 15:19:34 +0200

    Initial project setup

---
+++ ersanSwiftyLib/ersanSwiftyLib.h
@@ -0,0 +1,18 @@
+//
+//  ersanSwiftyLib.h
+//  ersanSwiftyLib
+//
+//  Created by Ersan on 16/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ersanSwiftyLib.
+FOUNDATION_EXPORT double ersanSwiftyLibVersionNumber;
+
+//! Project version string for ersanSwiftyLib.
+FOUNDATION_EXPORT const unsigned char ersanSwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ersanSwiftyLib/PublicHeader.h>
+
+

b5c296a24c871e95f3bc0165a537371b397b8992
Ersan <ersan@ersans-mbp.mynet>
2022-01-17 15:48:53 +0200

    Added test cases

---
+++ ersanSwiftyLib/SwiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyLib.swift
+//  ersanSwiftyLib
+//
+//  Created by Ersan on 17/01/2022.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
